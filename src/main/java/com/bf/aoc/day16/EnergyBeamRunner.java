27ee116520a7b9f0f7264b2cd21683d8af11ee3a
Kyle Jackson <kyle.jackson@benefitfocus.com>
2023-12-19 23:43:34 -0500

    day 16

---
+++ src/main/java/com/bf/aoc/day16/EnergyBeamRunner.java
@@ -0,0 +1,73 @@
+package com.bf.aoc.day16;
+
+import com.bf.aoc.Runner;
+import com.bf.aoc.day10.Direction;
+
+import java.util.List;
+
+public class EnergyBeamRunner extends Runner {
+    static {
+        DAY=16; TEST=false; PART=2;
+    }
+
+    public static void main(String[] args) {
+        EnergyBeamRunner r = new EnergyBeamRunner();
+        log.info("Result: {}", isPart1() ? r.part1(input()) : r.part2(input()));
+    }
+
+    @Override
+    public long part1(List<String> lines) {
+        EnergyGrid grid = new EnergyGrid(lines);
+        Beam beam = new Beam(new Coordinates(1,1), Direction.E);
+        beam.traverse(grid);
+        return grid.countEnergizedCells();
+    }
+
+    @Override
+    public long part2(List<String> lines) {
+        EnergyGrid grid = new EnergyGrid(lines);
+        long best = 0;
+        for (int r=1; r<grid.rows; r++) {
+            Beam beam = new Beam(new Coordinates(r,1), Direction.E);
+            beam.traverse(grid);
+            long count = grid.countEnergizedCells();
+            grid.reset();
+            log.info("{}[{},{}]E = {}", (count > best ? "****" : "\t"), r, 1, count);
+            if (count > best) {
+                best = count;
+            }
+        }
+        for (int r=1; r<grid.rows; r++) {
+            Beam beam = new Beam(new Coordinates(r, grid.cols-1), Direction.W);
+            beam.traverse(grid);
+            long count = grid.countEnergizedCells();
+            grid.reset();
+            log.info("{}[{},{}]W = {}", (count > best ? "****" : "\t"), r, grid.cols-1, count);
+            if (count > best) {
+                best = count;
+            }
+        }
+        for (int c=1; c< grid.cols; c++) {
+            Beam beam = new Beam(new Coordinates(1,c), Direction.S);
+            beam.traverse(grid);
+            long count = grid.countEnergizedCells();
+            grid.reset();
+            log.info("{}[{},{}]S = {}", (count > best ? "****" : "\t"), 1, c, count);
+            if (count > best) {
+                best = count;
+            }
+        }
+        for (int c=1; c< grid.cols; c++) {
+            Beam beam = new Beam(new Coordinates(grid.rows-1, c), Direction.N);
+            beam.traverse(grid);
+            long count = grid.countEnergizedCells();
+            grid.reset();
+            log.info("{}[{},{}]N = {}", (count > best ? "****" : "\t"), grid.rows-1, c, count);
+            if (count > best) {
+                best = count;
+            }
+        }
+        return best;
+    }
+
+}
---
+++ src/main/java/com/bf/aoc/day16/EnergyGrid.java
@@ -0,0 +1,52 @@
+package com.bf.aoc.day16;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+import java.util.List;
+
+public class EnergyGrid {
+    private final Table<Integer, Integer, Cell> grid = HashBasedTable.create();
+    final int rows, cols;
+
+    public EnergyGrid(List<String> lines) {
+        this.rows = lines.size()+1;
+        this.cols = lines.get(0).length()+1;
+        int row = 0;
+        for (String line : lines) {
+            row++;
+            int col = 0;
+            for (char c : line.toCharArray()) {
+                grid.put(row, ++col, new Cell(c));
+            }
+        }
+    }
+
+    public Cell getCell(Coordinates coordinates) {
+        return grid.get(coordinates.row, coordinates.col);
+    }
+
+    public Long countEnergizedCells() {
+        return grid.values().stream().filter(Cell::isEnergized).count();
+    }
+
+    public void reset() {
+        grid.values().forEach(Cell::reset);
+    }
+
+    public String printEnergyGrid(Coordinates coordinates) {
+        StringBuilder sb = new StringBuilder();
+        for (int r=1; r<rows; r++) {
+            for (int c=1; c<cols; c++) {
+                if (coordinates.row == r && coordinates.col == c) {
+                    sb.append("X");
+                } else {
+                    //noinspection DataFlowIssue
+                    sb.append(grid.get(r, c).energized ? "#" : ".");
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.append("\n").toString();
+    }
+}
